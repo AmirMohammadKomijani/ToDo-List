31ce67e627e51124a2407ef8fcc48bc74a62d285
AmirMohammadKomijani <amir.m.k8011@gmail.com>
2024-02-16 23:59:02 +0330

    <feat> : List DTOs

---
+++ ToDo-App/ToDo-App/Model/DTO/ListCreateDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace ToDo_App.Model.DTO
+{
+    public class ListCreateDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ ToDo-App/ToDo-App/Model/DTO/ListUpdateDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace ToDo_App.Model.DTO
+{
+    public class ListUpdateDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}

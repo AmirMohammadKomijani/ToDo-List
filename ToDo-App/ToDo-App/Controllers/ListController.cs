6c1f3a92d630af36bbf1e2e8836888dce61e798c
AmirMohammadKomijani <amir.m.k8011@gmail.com>
2024-02-16 23:22:56 +0330

    <Setup> : initialize project

---
+++ ToDo-App/ToDo-App/Controllers/ListController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToDo_App.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ListController : ControllerBase
+    {
+    }
+}

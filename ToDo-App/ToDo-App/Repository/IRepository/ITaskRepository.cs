09e39aeddd9bb7aac6be9bcfe96c46f889a8c55c
AmirMohammadKomijani <amir.m.k8011@gmail.com>
2024-02-17 11:08:22 +0330

    <feat> : Tasks Repository

---
+++ ToDo-App/ToDo-App/Repository/IRepository/ITaskRepository.cs
@@ -0,0 +1,9 @@
+﻿using ToDo_App.Model;
+
+namespace ToDo_App.Repository.IRepository
+{
+    public interface ITaskRepository : IRepository<Tasks>
+    {
+        Task<Tasks> Update(Tasks entity);
+    }
+}

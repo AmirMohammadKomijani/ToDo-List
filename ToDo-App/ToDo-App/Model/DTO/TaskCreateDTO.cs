5cb0e944fc4e1492bac1928acfdc1cd33baa02eb
AmirMohammadKomijani <amir.m.k8011@gmail.com>
2024-02-17 10:59:49 +0330

    <feat> : Task DTOs

---
+++ ToDo-App/ToDo-App/Model/DTO/TaskCreateDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDo_App.Model.DTO
+{
+    public class TaskCreateDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Starred { get; set; }
+        public bool Checked { get; set; }
+        public DateTime DueDate { get; set; }
+        [Required]
+        public int ListId { get; set; }
+    }
+}
--- ToDo-App/ToDo-App/Model/Tasks.cs
+++ ToDo-App/ToDo-App/Model/Tasks.cs
@@ -1,8 +1,16 @@
-﻿namespace ToDo_App.Model
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ToDo_App.Model
 {
     public class Tasks
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        [ForeignKey("list")]
+        public int ListId { get; set; }
+        public Lists list { get; set; }
         public string Name { get; set; }
         public bool Starred { get; set; }
         public bool Checked { get; set; }

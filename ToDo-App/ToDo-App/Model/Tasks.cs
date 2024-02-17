5aa026bc6cc115a61c5029345e66665b8713bb1e
AmirMohammadKomijani <amir.m.k8011@gmail.com>
2024-02-17 10:48:54 +0330

    <feat> : Tasks Model designed

---
+++ ToDo-App/ToDo-App/Model/Tasks.cs
@@ -0,0 +1,11 @@
+﻿namespace ToDo_App.Model
+{
+    public class Tasks
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Starred { get; set; }
+        public bool Checked { get; set; }
+        public DateTime DueDate { get; set; }
+    }
+}

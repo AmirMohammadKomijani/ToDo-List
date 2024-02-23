a40c56efd807d634c6c001097c76dd11d3f75d86
AmirMohammadKomijani <amir.m.k8011@gmail.com>
2024-02-23 20:59:13 +0330

    <feat> : dockerize the .net 6.0 + SQL and networking them.

--- ToDo-App/ToDo-App/Program.cs
+++ ToDo-App/ToDo-App/Program.cs
@@ -20,6 +20,8 @@ builder.Services.AddAutoMapper(typeof(MappingConfig));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAuthorization();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
